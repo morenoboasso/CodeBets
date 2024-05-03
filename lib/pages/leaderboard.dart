632f63c4fbbcb7e5da97ab845d451c7678377c03
Moreno <mirmoccnt@gmail.com>
2024-05-03 16:06:47 +0200

    add firebase login + bottombad

---
+++ lib/pages/leaderboard.dart
@@ -0,0 +1,11 @@
+// leaderboard_page.dart
+import 'package:flutter/material.dart';
+
+class LeaderboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Pagina per la classifica'),
+    );
+  }
+}
---
+++ lib/pages/ping_pong.dart
@@ -0,0 +1,11 @@
+// ping_pong_page.dart
+import 'package:flutter/material.dart';
+
+class PingPongPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Pagina per il Ping Pong'),
+    );
+  }
+}
