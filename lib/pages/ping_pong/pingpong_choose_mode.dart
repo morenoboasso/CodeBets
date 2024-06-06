b3352170756c3b44d10b25265c7afc4631922130
Moreno <moreno.boasso@codeploy.it>
2024-06-06 11:23:52 +0200

    chooseMode UI

--- lib/pages/ping_pong/pingpong_choose_mode.dart
+++ lib/pages/ping_pong/pingpong_choose_mode.dart
@@ -1,4 +1,5 @@
 import 'package:codebets/pages/ping_pong/pp_team_creation.dart';
+import 'package:codebets/style/color_style.dart';
 import 'package:flutter/material.dart';
 
 class ModeSelectionPage extends StatelessWidget {
@@ -9,7 +10,6 @@ class ModeSelectionPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          // Sfondo
           SizedBox.expand(
             child: Image.asset(
               'assets/bg.png',
@@ -20,37 +20,114 @@ class ModeSelectionPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
+                _buildModeButton(
+                  context: context,
+                  label: ' 1 vs 1 ',
+                  icon: Icons.person,
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const TeamCreationPage(players: 2)),
+                      MaterialPageRoute(
+                        builder: (context) => const TeamCreationPage(players: 2),
+                      ),
                     );
                   },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                    textStyle: const TextStyle(fontSize: 24),
-                  ),
-                  child: const Text('1 vs 1'),
+                  borderColor: ColorsBets.blueHD,
                 ),
-                const SizedBox(height: 20),
-                ElevatedButton(
+                const SizedBox(height: 30),
+                const Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: ColorsBets.blackHD,
+                        thickness: 1.5,
+                        indent: 25,
+                        endIndent: 5,
+                      ),
+                    ),
+                    Text(
+                      'Scegli Modalità',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: ColorsBets.blackHD,
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: ColorsBets.blackHD,
+                        thickness: 1.5,
+                        indent: 5,
+                        endIndent: 25,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                _buildModeButton(
+                  context: context,
+                  label: ' 2 vs 2 ',
+                  icon: Icons.people,
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const TeamCreationPage(players: 4)),
+                      MaterialPageRoute(
+                        builder: (context) => const TeamCreationPage(players: 4),
+                      ),
                     );
                   },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                    textStyle: const TextStyle(fontSize: 24),
-                  ),
-                  child: const Text('2 vs 2'),
+                  borderColor: ColorsBets.orangeHD,
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildModeButton({
+    required BuildContext context,
+    required String label,
+    required IconData icon,
+    required VoidCallback onPressed,
+    required Color borderColor,
+  }) {
+    return Material(
+      elevation: 3.0,
+      shadowColor: Colors.black.withOpacity(0.7),
+      borderRadius: BorderRadius.circular(15),
+      child: OutlinedButton(
+        onPressed: onPressed,
+        style: OutlinedButton.styleFrom(
+          backgroundColor: ColorsBets.whiteHD,
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          textStyle: const TextStyle(fontSize: 24),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          side: BorderSide(
+            color: borderColor,
+            width: 2.5,
+          ),
+          foregroundColor: borderColor,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, size: 30, color: borderColor),
+            const SizedBox(width: 10),
+            Text(
+              label,
+              style: TextStyle(
+                color: borderColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Icon(icon, size: 30, color: borderColor),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/ping_pong/pp_game_points.dart
+++ lib/pages/ping_pong/pp_game_points.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PointsPage extends StatefulWidget {
   final String team1Name;
@@ -28,38 +29,71 @@ class _PointsPageState extends State<PointsPage> {
   int _team1Points = 0;
   int _team2Points = 0;
   int _currentSet = 1;
-  int _maxPoints = 11; // Default to 11 points per set
+  int _team1SetsWon = 0;
+  int _team2SetsWon = 0;
+  late int _maxPoints;
+
+  @override
+  void initState() {
+    super.initState();
+    _maxPoints = widget.pointsPerSet;
+
+    // Blocca l'orientamento in orizzontale
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    // Ripristina l'orientamento predefinito
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Points'),
+        title: const Text('Points'),
       ),
       body: Row(
         children: [
-          _buildTeamColumn(
-            teamName: widget.team1Name,
-            teamColor: widget.team1Color,
-            teamPoints: _team1Points,
-            onPressedAdd: () {
-              _incrementPoints(1);
-            },
-            onPressedRemove: () {
-              _decrementPoints(1);
-            },
+          Expanded(
+            flex: 3,
+            child: _buildTeamColumn(
+              teamName: widget.team1Name,
+              teamColor: widget.team1Color,
+              teamPoints: _team1Points,
+              onPressedAdd: () {
+                _incrementPoints(1);
+              },
+              onPressedRemove: () {
+                _decrementPoints(1);
+              },
+            ),
           ),
-          _buildScoreColumn(),
-          _buildTeamColumn(
-            teamName: widget.team2Name,
-            teamColor: widget.team2Color,
-            teamPoints: _team2Points,
-            onPressedAdd: () {
-              _incrementPoints(2);
-            },
-            onPressedRemove: () {
-              _decrementPoints(2);
-            },
+          Expanded(
+            flex: 1,
+            child: _buildScoreColumn(),
+          ),
+          Expanded(
+            flex: 3,
+            child: _buildTeamColumn(
+              teamName: widget.team2Name,
+              teamColor: widget.team2Color,
+              teamPoints: _team2Points,
+              onPressedAdd: () {
+                _incrementPoints(2);
+              },
+              onPressedRemove: () {
+                _decrementPoints(2);
+              },
+            ),
           ),
         ],
       ),
@@ -73,52 +107,57 @@ class _PointsPageState extends State<PointsPage> {
     required VoidCallback onPressedAdd,
     required VoidCallback onPressedRemove,
   }) {
-    return Expanded(
-      child: Container(
-        color: teamColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              teamName,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Points: $teamPoints',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: onPressedAdd,
-                  child: Icon(Icons.add),
-                ),
-                ElevatedButton(
-                  onPressed: onPressedRemove,
-                  child: Icon(Icons.remove),
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Container(
+      color: teamColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            teamName,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Points: $teamPoints',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: onPressedRemove,
+                child: Icon(Icons.remove),
+              ),
+              ElevatedButton(
+                onPressed: onPressedAdd,
+                child: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildScoreColumn() {
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Set $_currentSet',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Set $_currentSet',
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 20),
+        Text(
+          '${widget.team1Name} Sets: $_team1SetsWon',
+          style: TextStyle(fontSize: 20),
+        ),
+        Text(
+          '${widget.team2Name} Sets: $_team2SetsWon',
+          style: TextStyle(fontSize: 20),
+        ),
+      ],
     );
   }
 
@@ -127,11 +166,22 @@ class _PointsPageState extends State<PointsPage> {
       if (team == 1) {
         if (_team1Points < _maxPoints) {
           _team1Points++;
+          if (_team1Points == _maxPoints) {
+            _team1SetsWon++;
+            _resetPoints();
+          }
         }
       } else if (team == 2) {
         if (_team2Points < _maxPoints) {
           _team2Points++;
+          if (_team2Points == _maxPoints) {
+            _team2SetsWon++;
+            _resetPoints();
+          }
         }
+      }
+      if (_currentSet > widget.numberOfSets) {
+        _showWinnerDialog();
       }
     });
   }
@@ -144,5 +194,40 @@ class _PointsPageState extends State<PointsPage> {
         _team2Points--;
       }
     });
+  }
+
+  void _resetPoints() {
+    _team1Points = 0;
+    _team2Points = 0;
+    _currentSet++;
+  }
+
+  void _showWinnerDialog() {
+    String winner;
+    if (_team1SetsWon > _team2SetsWon) {
+      winner = widget.team1Name;
+    } else if (_team2SetsWon > _team1SetsWon) {
+      winner = widget.team2Name;
+    } else {
+      winner = 'No one, it\'s a tie';
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Match Over'),
+          content: Text('$winner wins the match!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
